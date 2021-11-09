427ef0ac0ddfc8269a2b19adaabcfb66a9d3146e
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-09 16:29:40 +0530

    ADDED : If user message has happy or sad return the mood accordingly

---
+++ src/main/java/com/bridgeladz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,31 @@
+package com.bridgeladz.moodanalyzer;
+
+/****************************************************************************************
+ * @author Pavani
+ *
+ * This class has all the methods that defines the project.
+ * This class methods returns sad or happy expression according to the methods conditions.
+ * Specifying customs exceptions for uncertain inputs.
+ ****************************************************************************************/
+
+public class MoodAnalyzer {
+    /**
+     * Name : analyzeMood
+     *
+     * Description : Analyzing mood happy or sad.
+     *
+     * @param userMessage //user given message.
+     * @return //happy or sad.
+     *
+     * Algorithm : If user given message has happy or sad written in it.
+     * then returning the respective matching string i.e., either happy or sad.
+     */
+    public String analyzeMood(String userMessage) {
+        if(userMessage.contains("sad")) {
+            return "sad";
+        }
+        else {
+            return "happy";
+        }
+    }
+}
--- src/main/java/com/bridgeladz/moodanalyzer/MoodAnalyzerMain.java
+++ src/main/java/com/bridgeladz/moodanalyzer/MoodAnalyzerMain.java
@@ -1,8 +1,16 @@
 package com.bridgeladz.moodanalyzer;
 
+/**************************************************************
+ * @author Pavani
+ *
+ * This is main class for mood analyzer project.
+ * From this class we are calling methods of mood analyzer class.
+ **************************************************************/
+
 public class MoodAnalyzerMain {
     public static void main(String[] args) {
-        // Displaying Welcome Message
-        System.out.println("Welcome To Mood Analyzer Program");
+        MoodAnalyzer moodAnalyserObj = new MoodAnalyzer();
+        String userMessage = "Today i am sad";
+        System.out.println(moodAnalyserObj.analyzeMood(userMessage));
     }
 }

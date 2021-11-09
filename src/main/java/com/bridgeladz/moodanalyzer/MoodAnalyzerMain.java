6f81c7dc5670d1e216666658c15ecdc5ec0b113f
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-09 16:13:11 +0530

    ADDED : Welcome To Mood Analyzer Program

---
+++ src/main/java/com/bridgeladz/moodanalyzer/MoodAnalyzerMain.java
@@ -0,0 +1,8 @@
+package com.bridgeladz.moodanalyzer;
+
+public class MoodAnalyzerMain {
+    public static void main(String[] args) {
+        // Displaying Welcome Message
+        System.out.println("Welcome To Mood Analyzer Program");
+    }
+}
